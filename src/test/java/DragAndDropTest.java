230ce9a6b5c3312795e6847a23cd8155f57038bb
mtx <m33wwe@gmail.com>
2023-09-20 15:15:50 +0300

    add fix

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class DragAndDropTest {
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragAndDropTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void dragAndDropTest() {
+        open("/drag_and_drop");
+        // Перенести прямоугольник А на место В
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        $("#column-a").dragAndDrop(to($("#column-b")));
+        // Проверить, что прямоугольники действительно поменялись
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
 }
--- src/test/java/OpenTheEnterprisePageTest.java
+++ src/test/java/OpenTheEnterprisePageTest.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class OpenTheEnterprisePageTest {
+import org.junit.jupiter.api.BeforeAll;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class OpenTheEnterprisePageTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void openPageAndFoundHeaderTest() {
+        open(Configuration.baseUrl);
+        // найти в меню Solutions -> Enterprise
+        $(byTagAndText("button", "Solutions")).hover();
+        // перейти на страницу Enterprise
+        $(byText("Enterprise")).click();
+        // проверить, что открылась нужная страница
+        $(".application-main").shouldHave(text("Build like the best"));
+    }
 }
